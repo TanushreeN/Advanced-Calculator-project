8e623bb176c4e6fb6b11a865675b48b948357af7
TanushreeN <tanushree4016@gmail.com>
2024-02-27 20:34:56 +0530

    started basic of project

--- calc.js
+++ calc.js
@@ -4,6 +4,17 @@
     </head>
     
     <body>
+        <form>
+            <table>
+                <tbody>
+                    <tr>
+                    <td colspan="3"><input type="text" id="result"readonly></td>
+                    <td><input type="button" value="C" onclick="cl()"/></td>
+                    </tr>
+                
+                </tbody>
+            </table>
+        </form>
 
     </body>
 </html>
